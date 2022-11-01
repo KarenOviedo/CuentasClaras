b8a49c6f5110ce983334644f2165e803fc9d102a
Karen Oviedo <krn-oviedo@hotmail.com>
2022-11-01 01:10:39 -0600

    Carga inicial

---
+++ data.js
@@ -0,0 +1,8 @@
+var strCodCliente = "1234";
+var pass = "1234";
+var rememberMe = false;
+
+var strDomicilio = "Damianas 1020, Independencia, Guadalajara. Guadalajara, Jalisco.";
+var strTelefono = "+52 1 442 280 0621";
+var strCorreo = "karen.oviedoa@gmail.com";
+
